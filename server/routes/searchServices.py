ca024801596713406f84dd79aec21dc14f04f1e1
ivanpan0626 <ivanpan0626@gmail.com>
2024-07-27 20:50:59 -0400

    pie chart for win rate

--- server/routes/searchServices.py
+++ server/routes/searchServices.py
@@ -9,7 +9,7 @@ def organizeLeaderboard(chal_df, summonerGameNames, summonerTagLines, summonerIc
     chal_df_new.insert(0, 'summonerGameName', summonerGameNames)
     chal_df_new.insert(1, 'summonerTagLine', summonerTagLines)
     chal_df_new.insert(2, 'win_rate', (chal_df['wins']/(chal_df['wins']+chal_df['losses'])).apply('{:.0%}'.format))
-    chal_df_new.insert(3, 'winloss', chal_df['wins'].astype(str) + '/' + chal_df['losses'].astype(str))
+    chal_df_new.insert(3, 'winloss', chal_df['wins'].astype(str) + 'W/' + chal_df['losses'].astype(str) +'L')
     chal_df_new.insert(4, 'profileIconId', summonerIcons)
     chal_df_new.insert(5, 'summonerLevel', summonerLevels)
     leaderboard = chal_df_new.to_dict(orient='records')
